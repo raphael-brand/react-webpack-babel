1bad885e08ee997f1ad20f30c02f97d1aa5c18e6
Raphael Brand <info@raphaelbrand.de>
2020-03-05 09:14:58 +0100

    add DOM test

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import './setupTests';
+import App from './app/App';
+
 describe('My Test Suite', () => {
     it('My Test Case', () => {
         expect(true).toBe(true);
+    });
+
+    it('renders without crashing', () => {
+        shallow(<App />);
     });
 });
