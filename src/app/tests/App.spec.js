c288d76218d1f73363f4b30c420d71df023133ef
Raphael Brand <info@raphaelbrand.de>
2020-03-05 11:22:31 +0100

    add Test folder

---
+++ src/app/tests/App.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import './setupTests';
+import App from './../App';
+
+describe('My Test Suite', () => {
+    it('My Test Case', () => {
+        expect(true).toBe(true);
+    });
+
+    it('renders without crashing', () => {
+        shallow(<App />);
+    });
+});
